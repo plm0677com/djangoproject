6ff5c226c4b39665fef00fbc9f58a40c36d01d56
plm0677com <plm0677@163.com>
2020-11-30 22:52:19 +0800

    git push

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Board
+
 
 # Create your views here.
+
+def home(request):
+    boards = Board.objects.all()
+    # Board.objects.all()
+    # boards_names = list()
+    # for board in boards:
+    #     boards_names.append(board)
+    # response_html = '<br>'.join(boards_names)
+    return render(request, 'home.html', {"boards": boards})
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls import url
+from boards import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('^$', views.home, name='home'),
+    path('admin/', admin.site.urls)
 ]
